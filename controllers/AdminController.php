ae3601731d46770db7e0707e63d6dc6cde0ff835
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-11-12 05:19:42 -0500

    Create AdminController.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

---
+++ controllers/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace gm\humhub\modules\effects\controllers;
+
+use Yii;
+use gm\humhub\modules\effects\Module;
+use humhub\modules\admin\components\Controller;
+/**
+ * @property Module $module
+ */
+class AdminController extends Controller
+{
+    public function actionIndex()
+    {
+        $model = $this->module->getConfiguration();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            $this->view->saved();
+        }
+
+        return $this->render('index', [
+            'model' => $model,
+        ]);
+    }
+}
