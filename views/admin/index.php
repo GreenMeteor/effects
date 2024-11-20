870da8f15d72dd6e1f42bc759021b92593ad6591
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-11-20 15:54:51 -0500

    Update index.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- views/admin/index.php
+++ views/admin/index.php
@@ -8,6 +8,18 @@ use yii\helpers\Html;
 
 $this->title = Yii::t('EffectsModule.base', 'Effects Settings');
 $this->params['breadcrumbs'][] = $this->title;
+
+// Determine which effect is enabled
+$enabledEffect = null;
+if ($model->enableSakuraFall) {
+    $enabledEffect = 'enableSakuraFall';
+} elseif ($model->enableSnowfall) {
+    $enabledEffect = 'enableSnowfall';
+} elseif ($model->enableLeaffall) {
+    $enabledEffect = 'enableLeaffall';
+} elseif ($model->enableRainfall) {
+    $enabledEffect = 'enableRainfall';
+}
 ?>
 
 <div class="panel panel-default">
@@ -17,19 +29,28 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="panel-body">
         <?php if (Yii::$app->session->hasFlash('success')): ?>
             <div class="alert alert-success">
-            <?= Yii::$app->session->getFlash('success') ?>
-        </div>
+                <?= Yii::$app->session->getFlash('success') ?>
+            </div>
         <?php endif; ?>
 
         <?php $form = ActiveForm::begin(); ?>
-            <?= $form->field($model, 'enableSnowfall')->checkbox() ?>
-            <?= $form->field($model, 'enableSakuraFall')->checkbox() ?>
-            <?= $form->field($model, 'enableLeaffall')->checkbox() ?>
-            <?= $form->field($model, 'enableRainfall')->checkbox() ?>
 
-            <div class="form-group">
-                <?= Html::submitButton(Yii::t('EffectsModule.base', 'Save'), ['class' => 'btn btn-primary']) ?>
-            </div>
+        <?= $form->field($model, 'enableSnowfall')->checkbox([
+            'disabled' => $enabledEffect !== null && $enabledEffect !== 'enableSnowfall',
+        ]) ?>
+        <?= $form->field($model, 'enableSakuraFall')->checkbox([
+            'disabled' => $enabledEffect !== null && $enabledEffect !== 'enableSakuraFall',
+        ]) ?>
+        <?= $form->field($model, 'enableLeaffall')->checkbox([
+            'disabled' => $enabledEffect !== null && $enabledEffect !== 'enableLeaffall',
+        ]) ?>
+        <?= $form->field($model, 'enableRainfall')->checkbox([
+            'disabled' => $enabledEffect !== null && $enabledEffect !== 'enableRainfall',
+        ]) ?>
+
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('EffectsModule.base', 'Save'), ['class' => 'btn btn-primary']) ?>
+        </div>
         <?php ActiveForm::end(); ?>
     </div>
 </div>
